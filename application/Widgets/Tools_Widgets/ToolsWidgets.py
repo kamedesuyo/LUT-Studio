b54e7eaf08cd37367db725fdd0d188df54ebb554
Nagata Toshiya <gp23a103@oecu.jp>
2024-09-17 04:33:49 +0900

    [add]optionid2nameゲッターの追加,ImageInterfaceへの接続追加

--- application/Widgets/Tools_Widgets/ToolsWidgets.py
+++ application/Widgets/Tools_Widgets/ToolsWidgets.py
@@ -1,21 +1,25 @@
 import customtkinter
+from application.Image_Interface.ImageInterface import ImageInterface
 from typing import Literal
 from CTkMessagebox import CTkMessagebox
+
 WIDTH = 896
 HEIGHT = 186
 
 class ToolsWidgets(customtkinter.CTkFrame):
     def __init__(self,root):
-        super().__init__(root,
+        super().__init__(master=root,
                          width=WIDTH,
                          height=HEIGHT,
                          corner_radius=0,
                          )
         self.root = root
+        # インターフェース接続
+        self.ImageInterface = ImageInterface()
         # 初期値
         self.current_option = customtkinter.IntVar(value=0)
-        self._sliders = []
-        self._value_labels = []  # スライダーの値表示用のラベルを管理
+        self._sliders_list = []
+        self._value_labels_list = []  # スライダーの値表示用のラベルを管理
 
         # slider info 初期化
         self.setting_slider_info()
@@ -63,27 +67,26 @@ class ToolsWidgets(customtkinter.CTkFrame):
             if i == 0:
                 _button.invoke(True)
 
-    # sliderの再描画
+    # オプションごとのslider描画
     def reload_slider(self):
         self.draw_sliders(self.slider_values[self.get_current_option_id()])
     
-    # slider_valuesのvalueを受け取りsliderに適応、値ラベルと共に描画
+    # slider_valuesのvalueを受け取りsliderに適応、値ラベル共に描画
     def draw_sliders(self, slider_values: dict):
         # スライダーとラベルを削除
-        for slider in self._sliders:
+        for slider in self._sliders_list:
             slider.destroy()
-        for label in self._value_labels:
+        for label in self._value_labels_list:
             label.destroy()
 
         x = 150
         y = 35
         padx = 100
-        self._sliders = []
-        self._value_labels = []
-        self._reset_button = []
+        self._sliders_list = []
+        self._value_labels_list = []
 
+        # スライダー要素の作成(reset,slider,label)
         for i, (color, val) in enumerate(slider_values.items()):
-            # スライダーの作成
             
             # スライダーの値をリセットするボタン
             button = customtkinter.CTkButton(self,text="reset",width=10,height=10,command=lambda color=color: self.reset_slider_value(color))
@@ -99,27 +102,30 @@ class ToolsWidgets(customtkinter.CTkFrame):
                 height=120,
                 variable=val,
                 orientation="vertical",
-                command=lambda value, c=color, v=val: self.update_label(c, v),
+                command=lambda value, c=color, v=val: self.update(c, v),
             )
             slider.place(x=x + padx * i, y=y)
-            self._sliders.append(slider)
+            self._sliders_list.append(slider)
 
             # スライダーの値を表示するラベル
             label = customtkinter.CTkLabel(self, text=f"{color}: {val.get():.2f}")
-            label.place(x=x + padx * i-10, y=y + 120)  # スライダーの下に配置
-            self._value_labels.append(label)
+            # スライダーの下に配置
+            label.place(x=x + padx * i-10, y=y + 120) 
+            self._value_labels_list.append(label)
 
     # sliderの値をリセット
     def reset_slider_value(self,color):
         slider_value = self.slider_values[self.get_current_option_id()][color]
         slider_value.set(0)
-        self.update_label(color,slider_value)
+        self.update(color,slider_value)
 
-    # スライダーの値が変わるたびにラベルを更新
-    def update_label(self, color, variable):
-        for label in self._value_labels:
+    # スライダーの値が変わるたびにラベルを更新, ここで画像処理が走る
+    def update(self, color, variable):
+        for label in self._value_labels_list:
             if label.cget("text").startswith(f"{color}:"):
                 label.configure(text=f"{color}: {variable.get():.2f}")
+                self.ImageInterface.apply_filter(color,self.get_current_option_id(),variable.get())
+                
     # すべてのスライダーをリセット
     def reset_all_sliders(self):
         for sliders in self.slider_values.values():
@@ -138,14 +144,21 @@ class ToolsWidgets(customtkinter.CTkFrame):
     def get_current_option_name(self) -> str:
         return self.options[self.current_option.get()]
 
+    def get_optionid2name(self,id):
+        return self.options[id]
+
+    def get_slider_values(self)->list:
+        sliders = []
+        for op_id, sliders in self.slider_values.items():
+            for color, slider in sliders.items():
+                sliders.append(slider)
+        return sliders
+    
     def print_slider_values(self):
         for op_id, sliders in self.slider_values.items():
             print(self.options[op_id])
             for color, slider in sliders.items():
                 print(f"{color}: {slider.get():.2f}")
-    
-    def get_slider_values(self):
-        return self.slider_values
     # 引数でリセットスコープを指定
     def check_reset(self,reset_scope:Literal["All","CurrentOption"]):
         # scopeによって変更
