1ba161f54aab4dd396e3131ac8c9be614fa15d78
Nagata Toshiya <gp23a103@oecu.jp>
2024-09-19 06:35:50 +0900

    [add]filter機能の作成まで

--- application/Image_Interface/ImageInterface.py
+++ application/Image_Interface/ImageInterface.py
@@ -1,46 +1,58 @@
 from PIL import Image
 import customtkinter
-from tkinterdnd2 import TkinterDnD,DND_FILES
-import cv2
-# Singleton 
-class Singleton():
-    def __new__(cls, *args,**kargs):
-        if not hasattr(cls,"_instance"):
-            cls._instance = super(Singleton, cls).__new__(cls)
+from application.Image_Interface.filter import apply_gamma, apply_saturation, apply_contrast
+
+class Singleton:
+    _instance = None
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
         return cls._instance
-    
+
 class ImageInterface(Singleton):
     def __init__(self):
-        # ImageWidgets info
-        self.root: TkinterDnD.Tk|None = None
-        self.image_label:customtkinter.CTkLabel|None = None
-        self.widgets_size: tuple = None
-        self.image: Image.Image | None = None
-        self.image_path: str | None = None
-
-        # ToolWidgets
-        self.color: str | None = None
-        self.current_option_id: int = 1
-        self.value: float = 0
+        self.root = None
+        self.image_label = None
+        self.widgets_size = None
+        self.image = None
+        self.result_image = None
 
     def load_image(self, image: Image.Image, image_path: str, image_label, root, widgets_size: tuple):
         self.root = root
         self.image_label = image_label
         self.widgets_size = widgets_size
         self.image = image
-        self.image_path = image_path
+        self.result_image = image
 
-    def apply_filter(self, color: str, current_option_id: int, value: float):
-        self.color = color
-        self.current_option_id = current_option_id
-        self.value = value
-        
-        self.redraw_image_label()
+    def apply_filter(self, slider_values: dict[int, dict[str, float]]):
+        if self.image:
+            result_image = self.image
+            for op_id, sliders in slider_values.items():
+                R, G, B, Y = (sliders[color].get() for color in "RGBY")
+                match op_id:
+                    case 0:
+                        result_image = apply_saturation(result_image, R, G, B, Y)
+                    case 1:
+                        result_image = apply_contrast(result_image, R, G, B, Y)
+                    case 2:
+                        result_image = apply_gamma(result_image, R, G, B, Y)
+            self.result_image = result_image
+            self.redraw_image_label(result_image)
 
-    def redraw_image_label(self):
-        # 画像をCTkImageに変換
-        CTkimage = customtkinter.CTkImage(light_image=self.image, dark_image=self.image, size=self.widgets_size)
-        # image_labelを更新
+    def filter(self, color: str, current_option_id: int, op_slider_values: dict):
+        if self.image:
+            R, G, B, Y = (op_slider_values[color].get() for color in "RGBY")
+            match current_option_id:
+                case 0:
+                    result_image = apply_saturation(self.result_image, R, G, B, Y)
+                case 1:
+                    result_image = apply_contrast(self.result_image, R, G, B, Y)
+                case 2:
+                    result_image = apply_gamma(self.result_image, R, G, B, Y)
+            self.redraw_image_label(result_image)
+
+    def redraw_image_label(self, image):
+        CTkimage = customtkinter.CTkImage(light_image=image, dark_image=image, size=self.widgets_size)
         self.image_label.configure(image=CTkimage)
-        # 参照を保持する必要があるため、self.image_label.imageに保持
         self.image_label.image = CTkimage
+    
--- application/Widgets/Image_Widgets/ImageWidgets.py
+++ application/Widgets/Image_Widgets/ImageWidgets.py
@@ -9,48 +9,46 @@ HEIGHT = 504
 
 class ImageWidgets(customtkinter.CTkFrame):  
     def __init__(self, root: TkinterDnD.Tk):
-        super().__init__(root,
-                         width=WIDTH,
-                         height=HEIGHT,
-                         fg_color="#000000",
-                         corner_radius=0,
-                         )
+        super().__init__(root, width=WIDTH, height=HEIGHT, fg_color="#000000", corner_radius=0)
         self.root = root
-        # インターフェース接続
-        self.ImageInterface = ImageInterface()
-        # dndのバインド
-        self.enable_dnd()
-        self.draw_dnd_label()
+        self.ImageInterface = ImageInterface()  # インターフェース接続
+        self.label = None  # ラベルを初期化
+        self.image_label = None  # 画像ラベルを初期化
+        self.enable_dnd()  # D&Dを有効化
+        self.draw_dnd_label()  # D&D用ラベル描画
         self.pack_propagate(False)
 
-    # drop時処理: path取得→ open → image更新 → resize → labelで表示(初回でD&Dlabel削除) → ImageInterface
     def drop(self, event: TkinterDnD.DnDEvent):
         image_file_path = event.data[1:-1]  # path取得 両端にある{}除去
-        # open, resize, [create]image_labed
         try:
-            image = Image.open(image_file_path)
+            image_origin = Image.open(image_file_path)
         except Exception as e:
-            print(e)
-        image = resize_image(image, (WIDTH, HEIGHT))
-        self.draw_image_label(image)
-        self.ImageInterface.load_image(image,image_file_path,self.image_label,self.root,(WIDTH, HEIGHT))
+            print(f"Image open error: {e}")
+            return
 
-    def draw_image_label(self,image):
+        resized_image = resize_image(image_origin, (WIDTH, HEIGHT))
+        self.update_image_label(resized_image)
+        self.ImageInterface.load_image(image_origin, image_file_path, self.image_label, self.root, (WIDTH, HEIGHT))
+
+    def update_image_label(self, image):
         self.image = customtkinter.CTkImage(light_image=image, dark_image=image, size=(WIDTH, HEIGHT))
-        # [update]image_label [del]label
-        self.label.destroy()
-        self.image_label.destroy()
-        self.image_label = customtkinter.CTkLabel(self, image=self.image,text="")
+        
+        # labelが存在する場合はdestroyして新たに生成する
+        if self.label:
+            self.label.destroy()
+        if self.image_label:
+            self.image_label.destroy()
+
+        # 新しいimage_labelの生成
+        self.image_label = customtkinter.CTkLabel(self, image=self.image, text="")
         self.image_label.pack()
 
     def draw_dnd_label(self):
-        # d&d領域にlabel設置、image_labelの仮置き
-        self.label = customtkinter.CTkLabel(self.root, text="ここに画像をD&D", text_color="white", fg_color="#000000", font=(None,30))  # LabelをCTkLabelに変更
-        self.image_label = customtkinter.CTkLabel(self,text="")
-        self.label.place(relx=0.37, rely=0.37, anchor="center")
-        
+        # D&D領域にlabel設置、image_labelの仮置き
+        self.label = customtkinter.CTkLabel(self, text="ここに画像をD&D", text_color="white", fg_color="#000000", font=(None, 30))
+        self.label.place(relx=0.5, rely=0.5, anchor="center")
+
     def enable_dnd(self):
-        # tkinterのwrapperであるtkinterdnd2で提供されているメソッドを使用してD&Dを有効化。ウィジェットにdrop時のトリガーをセット
+        # tkinterdnd2の機能でD&Dを有効化し、ドロップイベントをバインド
         self.drop_target_register(DND_FILES)
         self.dnd_bind('<<Drop>>', self.drop)
-
